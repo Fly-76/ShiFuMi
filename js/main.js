3a0c84fdc47862506e9400659064843f3864891e
Franck LEUYER <f_leuyer@hotmail.com>
2020-07-02 16:21:28 +0200

    request for a user choice

--- js/main.js
+++ js/main.js
@@ -5,5 +5,7 @@ alert("Bonjour et bienvennue");
 
 
 // *** Get user name
-var userName = prompt("Veuillez entre votre nom");
+var userName = prompt("Veuillez entrer votre nom");
 
+// *** Input user choice
+var userChoice = prompt("Veuillez entrer choisir : Pierre, Feuille ou Ciseaux");
